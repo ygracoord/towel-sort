60bcb78ae43b6272ce5029522cad1099e7582102
ygracoord <ygra.pl@gmail.com>
2022-08-12 21:43:42 +0300

    feat: complete towel-sort

--- src/index.js
+++ src/index.js
@@ -2,5 +2,16 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let arr1 = [];
+  if (matrix === undefined) return [];
+   for (let i = 0; i < matrix.length; i++) {
+       for (let j = 0; j < matrix[i].length; j++){
+           if (i % 2 === 0) {
+               arr1.push(matrix[i][j]);
+           } else {
+               arr1.push(matrix[i][matrix[i].length-j-1]);
+           }
+       }
+   }
+   return matrix.length ? arr1 : [];
 }
